5ec24e636c484e4c67304bed2a621fc80d7503d1
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2020-11-26 18:02:14 +0300

    gradle:4.0.1

---
+++ app/src/main/java/ru/skillbranch/devintensive/repositories/PreferencesRepository.kt
@@ -0,0 +1,9 @@
+package ru.skillbranch.devintensive.repositories
+
+import ru.skillbranch.devintensive.models.Profile
+
+object PreferencesRepository {
+    fun getProfile(): Profile? {
+//     return null
+    }
+}
