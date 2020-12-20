6770eb5048469356b37bb04730880ceb7b9a2ec2
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2020-12-20 22:37:45 +0300

    Initial

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/main/MainActivity.kt
@@ -0,0 +1,27 @@
+package ru.skillbranch.devintensive.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.skillbranch.devintensive.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initTollBar()
+        initViews()
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        TODO("Not yet implemented")
+    }
+
+    private fun initViews() {
+        TODO("Not yet implemented")
+    }
+
+    private fun initTollBar() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/viewmodels/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ru.skillbranch.devintensive.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel(){
+    private val chatRepository = 
+}
