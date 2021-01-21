91c1311f73d94efc60fde8fff2428794f1d4a3d0
aleksey-knyazhev <aleksey.knyazhev@gmail.com>
2021-01-21 23:25:02 +0300

    Initial

--- app/src/main/java/ru/skillbranch/devintensive/ui/adapters/ChatAdapter.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/adapters/ChatAdapter.kt
@@ -4,9 +4,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import ru.skillbranch.avatarimageview.ui.custom.AvatarImageView
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_chat_single.*
 import ru.skillbranch.devintensive.R
 import ru.skillbranch.devintensive.models.data.ChatItem
 
@@ -27,13 +27,16 @@ class ChatAdapter : RecyclerView.Adapter<ChatAdapter.SingleViewHolder>() {
 
     override fun getItemCount(): Int = items.size
 
-    inner class SingleViewHolder(convertView: View) : RecyclerView.ViewHolder(convertView) {
+    inner class SingleViewHolder(convertView: View) : RecyclerView.ViewHolder(convertView), LayoutContainer {
         //val iv_avatar = convertView.findViewById<AvatarImageView>(R.id.iv_avatar_single)
         //val tv_title = convertView.findViewById<TextView>(R.id.tv_title_single)
 
+        override val containerView: View?
+            get() = itemView
+
         fun bind(item:ChatItem){
-            iv_avatar.setInitials(item.initials)
-            tv_title.text = item.shortDescription
+            iv_avatar_single.setInitials(item.initials)
+            tv_title_single.text = item.shortDescription
         }
     }
 }
